060961741db3829619c372d640f04267c3f84066
Serhii Huryn <segiy.gurin@gmail.com>
2025-04-21 12:45:08 +0200

    add all

---
+++ js/task-4.js
@@ -0,0 +1,21 @@
+const form = document.querySelector(".login-form");
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
+form.addEventListener("submit", handler);
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+});
+
+const user = {}
+
+function handler() {
+    if (email.value === "" || password.value === "") {
+        alert('All form fields must be filled in')
+    } else {
+        user.email = email.value.trim()
+        user.password = password.value.trim()
+        console.log(user);
+        form.reset()
+    }
+}
---
+++ js/task-5.js
@@ -0,0 +1,17 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
+
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+const body = document.querySelector("body");
+
+btn.addEventListener("click", handler);
+
+function handler() {
+    const color = getRandomHexColor();
+    span.textContent = color;
+    body.style.backgroundColor = color;
+}
